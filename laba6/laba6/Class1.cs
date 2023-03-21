98c294b93f2845957d48001f6c0153372e95a507
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-03-21 18:34:37 +0200

    Add files via upload

---
+++ laba6/laba6/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba6
+{
+    public class Param
+    {
+        public int h;
+        public Bitmap pic;
+    }
+   
+
+    public class ParamOneYearTree : Param
+    {
+        public ParamOneYearTree()
+        {
+            h = 50;
+            pic = new Bitmap("Без названия.jfif");
+        }
+    }
+    public class ParamTwoYearTree : Param
+    {
+        public ParamTwoYearTree()
+        {
+            h = 100;
+            pic = new Bitmap("Без названия.png");
+        }
+    }
+    public class Tree
+    {
+        public int x, y;
+        public Param p;
+        public Tree()
+        {
+            x = 0; y = 0;
+        }
+
+        public Tree(int x, int y, Param p)
+        {
+            this.p = p;
+            this.x = x;
+            this.y = y;
+        }
+        Random random = new Random();
+        public void Clear()
+        {
+
+        }
+        public void move()
+        {
+
+            if (x < 1000)
+            {
+                this.x += random.Next(1, 50);
+            }
+            else
+            {
+                x = 1;
+            }
+        }
+    }
+}
---
+++ laba6/laba6/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+        Graphics g;
+        Tree[] t = new Tree[500];
+        Param[] p = new Param[2];
+        List<Tree> plist = new List<Tree>();
+        ParamOneYearTree p1 = new ParamOneYearTree();
+        ParamTwoYearTree p2 = new ParamTwoYearTree();
+        int count = 0;
+        Random r = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (r.Next(1, 3) == 1)
+                plist.Add(new Tree(r.Next(10, 100), r.Next(10, 1500), p1));
+            else
+                plist.Add(new Tree(r.Next(10, 100), r.Next(10, 1500), p2));
+
+
+            g.DrawImage(plist[count].p.pic, new Rectangle(plist[count].x, plist[count].y, plist[count].p.h, 20));
+
+            foreach (Tree q in plist)
+            {
+
+                q.move();
+                g.Clear(Color.AliceBlue);
+                for (int i = 0; i < plist.Count; i++)
+                {
+                    g.DrawImage(plist[i].p.pic, new Rectangle(plist[i].x, plist[i].y, plist[i].p.h, 20));
+                }
+            }
+            if (count < 499)
+                count++;
+            else
+                timer1.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
